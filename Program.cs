10f729065c2c338ff74f6909611ffe5bf8b225d4
abc <abc@gmail.com>
2024-02-15 16:36:26 +0530

    assignment

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+Console.WriteLine("table of 5");
+int num = 5;
+for(int i=1;i<10;i++)
+{
+    Console.WriteLine("{0} * {1} = {2}",num,i,(num*i));
+}
